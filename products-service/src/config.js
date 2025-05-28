26e3278795c767fe80ffe472d900d70f430a2276
HabibSebasGJ <habibsebastiangj@gmail.com>
2025-05-27 19:16:17 -0500

    modifico el config para usar el JWT ilimitado que el admin facilito

--- products-service/src/config.js
+++ products-service/src/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  JWT_SECRET: process.env.JWT_SECRET || 'my-super-secret',
+  JWT_SECRET: process.env.JWT_SECRET || 'i3876538~i$5',
   PRODUCTS_TABLE: process.env.PRODUCTS_TABLE || 'Products',
 };
